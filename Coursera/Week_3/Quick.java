15ee03d22fc52e3d2fe65f48c10867843716ca3d
duke <dukeluo@outlook.com>
2018-01-15 20:43:41 +0800

    add Quicksort

---
+++ Coursera/Week_3/Quick.java
@@ -0,0 +1,92 @@
+/**
+  * 2.3 快速排序
+  * 快速排序
+  * quick sort
+  */
+
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdRandom;
+import java.util.Comparator;
+
+public class Quick {
+    public static Comparable select(Comparable[] a, int k) {
+        StdRandom.shuffle(a);
+        int lo = 0, hi = a.length-1;
+        while (hi > lo) {
+            int j = partition(a, lo, hi);
+            if (j < k)
+                lo = j + 1;
+            else if (j > k)
+                hi = j - 1;
+            else
+                return a[k];
+        }
+        return a[k];
+    }
+
+    public static void sort(Comparable[] a) {
+        StdRandom.shuffle(a);       //　消除对输入的依赖
+        sort(a, 0, a.length-1);
+    }
+
+    private static void sort(Comparable[] a, int lo, int hi) {
+        if (hi <= lo)
+            return ;
+        int j = partition(a, lo, hi);
+        sort(a, lo, j-1);
+        sort(a, j+1, hi);
+    }
+
+    private static int partition(Comparable[] a, int lo, int hi) {
+        int i = lo, j = hi+1;
+        Comparable v = a[lo];
+        while (true) {
+            while (less(a[++i], v))
+                if (i == hi)
+                    break;
+            while (less(v, a[--j]))
+                if (j == lo)
+                    break;
+            if (i >= j)
+                break;
+            exch(a, i, j);
+        }
+        exch(a, lo, j);
+        return j;
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    private static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++)
+            StdOut.print(a[i] + " ");
+        StdOut.println();
+    }
+
+    public static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; i++)
+            if (less(a[i], a[i-1]))
+                return false;
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String[] a = In.readStrings();
+        sort(a);
+        assert isSorted(a);
+        show(a);
+        StdOut.println("0 th: " + select(a, 0));
+        StdOut.println(a.length-1 + " th: " + select(a, a.length-1));
+    }
+}
+
+        
